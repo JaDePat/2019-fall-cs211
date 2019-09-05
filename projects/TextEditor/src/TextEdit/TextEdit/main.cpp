ba86914fa720496000890ee42e9dabd687a446fd
JaDePat <jdp664@humboldt.edu>
2019-09-04 19:46:38 -0700

    added code to allow the user to type in the window

--- projects/TextEditor/src/TextEdit/TextEdit/main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/main.cpp
@@ -21,7 +21,7 @@ int main(void)
 	noecho();
 	//nodelay(main_window, TRUE);
 	keypad(main_window, TRUE);
-	curs_set(0);
+	curs_set(TRUE);
 
 	//FUN STUFF HAPPENS HERE
 
@@ -46,8 +46,17 @@ int main(void)
 	//revert back to normal console mode
 	//nodelay(main_window, TRUE);
 	keypad(main_window, TRUE);
-	mvaddstr(1, 1, "Press any key to continue...");
-	char result = getch();
+	mvaddstr(1, 1, "Press Q to quit...");
+	move(2, 1);
+	int result = ' ';
+
+	//lets user type in the window
+	while (result != 'Q')
+	{
+		int result2 = getch();
+		addch(result2); 
+		result = result2;
+	}
 	endwin();
 
 }
