66d51cca99ef076af905477a3e50a8812c8abe93
JaDePat <jdp664@humboldt.edu>
2019-09-05 10:18:34 -0700

    #2 #3 #6 I added a file and edit text at the top of the text editor. The text editor now allows the user to delete characters with backspace.

--- projects/TextEditor/src/TextEdit/TextEdit/main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/main.cpp
@@ -13,50 +13,57 @@ int main(void)
 	main_window = initscr();
 
 	//take up most of the screen
-	getmaxyx(main_window, num_rows, num_cols);
+	/*getmaxyx(main_window, num_rows, num_cols);
 	resize_term(num_rows - 1, num_cols - 1);
-	getmaxyx(main_window, num_rows, num_cols);
+	getmaxyx(main_window, num_rows, num_cols);*/
+	wborder(main_window, 0, 0, 1161, 0, 0, 0, 0, 0);
+
+	//Checks if terminal can handle color. 
+	//Code from linuxjournal.com by Jim Hall
+	if (has_colors() == FALSE)
+	{
+		endwin();
+		printf("Your window does not support color\n");
+		exit(1);
+	}
+
+	start_color();
+	init_pair(1, COLOR_BLACK, COLOR_WHITE);
+	attron(COLOR_PAIR(1));
+	mvaddstr(0, 5, "FILE");
+	mvaddstr(0, 13, "EDIT");
+	attroff(COLOR_PAIR(1));
+
 
 	//turn off key echo
 	noecho();
 	//nodelay(main_window, TRUE);
-	keypad(main_window, TRUE);
+	//keypad(main_window, TRUE);
 	curs_set(TRUE);
-
-	//FUN STUFF HAPPENS HERE
-
-	for (int i = 0; i < num_cols; i++)
-	{
-		//top row
-		mvaddch(0, i, 1161);
-
-	}
-	for (int i = 0; i < num_rows; i++)
-	{
-		//left column
-		mvaddch(i+1, 0, '<');
-
-		//right column
-		mvaddch(i+1, num_cols - 1, '>');
-	}
 	
 	//tells curses to draw
 	refresh();
 
 	//revert back to normal console mode
 	//nodelay(main_window, TRUE);
-	keypad(main_window, TRUE);
-	mvaddstr(1, 1, "Press Q to quit...");
+	//keypad(main_window, TRUE);
+	mvaddstr(1, 1, "Press ESC to quit...");
 	move(2, 1);
 	int result = ' ';
 
 	//lets user type in the window
-	while (result != 'Q')
+	while (result != 27)
 	{
 		int result2 = getch();
-		addch(result2); 
+		if (result2 == 8)
+		{
+			printw("\b");
+			printw(" ");
+		}
+		addch(result2);
 		result = result2;
 	}
+
 	endwin();
 
 }
